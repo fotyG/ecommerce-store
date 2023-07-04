4cef09b4ebf6be1b01dca10dde601dfe4ed0db94
Sota Giorgadze <giorgadze@inbox.lv>
2023-07-04 19:51:48 +0300

    day 5 [9:20:32]

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { MouseEventHandler } from "react";
 import { useRouter } from "next/navigation";
 import { Expand, ShoppingCart } from "lucide-react";
 
@@ -8,15 +9,24 @@ import { Product } from "@/types";
 
 import Currency from "@/components/ui/currency";
 import IconButton from "@/components/ui/icon-button";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 interface ProductCardProps {
   data: Product;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  const previewModal = usePreviewModal();
   const router = useRouter();
+
   const handleClick = () => {
-    router.push(`/products/${data?.id}`);
+    router.push(`/product/${data?.id}`);
+  };
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.stopPropagation();
+
+    previewModal.onOpen(data);
   };
 
   return (
@@ -35,7 +45,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              onClick={() => {}}
+              onClick={onPreview}
               icon={
                 <Expand
                   size={20}
