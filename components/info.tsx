a4efcdfa8b2d61f98b13256d6bde09b0cde2d12b
Sota Giorgadze <giorgadze@inbox.lv>
2023-07-05 13:40:48 +0300

    fix addToCart from modal

--- components/info.tsx
+++ components/info.tsx
@@ -6,12 +6,18 @@ import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
 
 import { Product } from "@/types";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product;
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart();
+
+  const onAddToCart = () => {
+    cart.addItem(data);
+  };
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -35,7 +41,10 @@ const Info: React.FC<InfoProps> = ({ data }) => {
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
-        <Button className="flex items-center gap-x-3">
+        <Button
+          onClick={onAddToCart}
+          className="flex items-center gap-x-3"
+        >
           Add To Cart
           <ShoppingCart />
         </Button>
